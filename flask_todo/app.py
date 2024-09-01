2841373980b166a4607126418587a44054277012
Yuta Kouchi <152401488+yutaultra@users.noreply.github.com>
2024-09-01 21:01:07 +0900

    Update app.py

--- flask_todo/app.py
+++ flask_todo/app.py
@@ -68,4 +68,4 @@ def completed_tasks():
     return render_template('completed_tasks.html', todos=completed_todos)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
